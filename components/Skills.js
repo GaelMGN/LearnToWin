12ec2bccfb7b58f6b4e3413ef7dd27f95200defb
Gaël Moigne <gmoigne@live.fr>
2022-12-07 11:05:56 +0100

    fix skills

--- components/Skills.js
+++ components/Skills.js
@@ -2,22 +2,22 @@ import Image from "next/image";
 
 function Skills() {
   return (
-    <div className="flex flex-col items-center justify-center p-12 sm:mx-auto sm:max-w-full sm:flex-row">
-      <div className="flex flex-col items-center sm:max-w-[33%]">
+    <div className="flex flex-col justify-center p-12 sm:mx-auto sm:max-w-full sm:flex-row sm:justify-around">
+      <div className="sm:max-w-[30%] md:max-w-[25%]">
         <Image src={require("../src/assets/img/mascotte-torche.png")} />
-        <p className="py-8 text-center text-xl font-medium leading-6 text-gray-100">
+        <p className=" text-center text-xl font-medium leading-6 text-gray-100">
           Sois conseillé et accompagné par des coachs de performances certifiés
         </p>
       </div>
-      <div className="flex flex-col items-center sm:max-w-[33%]">
+      <div className="sm:max-w-[30%] md:max-w-[25%]">
         <Image src={require("../src/assets/img/mascotte-épée.png")} />
-        <p className="py-8 text-center text-xl font-medium leading-6 text-gray-100">
+        <p className=" text-center text-xl font-medium leading-6 text-gray-100">
           Forge tes compétences off-game (entretien physique, alimentation …)
         </p>
       </div>
-      <div className="flex flex-col items-center sm:max-w-[33%]">
+      <div className="sm:max-w-[30%] md:max-w-[25%]">
         <Image src={require("../src/assets/img/mascotte-mort.png")} />
-        <p className="py-8 text-center text-xl font-medium leading-6 text-gray-100">
+        <p className=" text-center text-xl font-medium leading-6 text-gray-100">
           Domine la concurrence in-game
         </p>
       </div>
